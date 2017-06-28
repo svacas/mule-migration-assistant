f1b4cdbb9c6a9bc2d0797b972e8c1034f2ebf084
Carlos Martinez Ledo <carlos.ledo@mulesoft.com>
2017-06-28 14:15:58 -0300

    Fixed http migration tasks (#25)

---
+++ src/test/java/com/mulesoft/tools/migration/usecase/MigrationJobApp3Test.java
@@ -0,0 +1,46 @@
+package com.mulesoft.tools.migration.usecase;
+
+import com.mulesoft.tools.migration.MigrationJob;
+import org.jdom2.Document;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static com.mulesoft.tools.migration.helper.DocumentHelper.getDocument;
+import static com.mulesoft.tools.migration.helper.DocumentHelper.restoreTestDocument;
+
+public class MigrationJobApp3Test {
+    private MigrationJob migrationJob;
+    private Document docRestoreFile;
+    private String USE_CASE_FILE_PATH = "src/test/resources/mule/apps/usecase03/use-case-03.xml";
+    private String TASKS_FILE_DIR = "src/test/resources/mule/tasks";
+
+    @Before
+    public void setUp() throws Exception {
+        ArrayList<String> filePath1 = new ArrayList<String>();
+        filePath1.add(USE_CASE_FILE_PATH);
+
+        migrationJob = new MigrationJob();
+        migrationJob.setOnErrorStop(Boolean.FALSE);
+        migrationJob.setDocuments(filePath1);
+        migrationJob.setBackUpProfile(Boolean.FALSE);
+        docRestoreFile = getDocument(filePath1.get(0));
+    }
+
+    @Test
+    public void jobWithTasksOnConfigFile() throws Exception {
+        ArrayList<String> files = new ArrayList<>(Arrays.asList(USE_CASE_FILE_PATH));
+
+        migrationJob.setDocuments(files);
+        migrationJob.setConfigFileDir(TASKS_FILE_DIR);
+        migrationJob.execute();
+    }
+
+    @After
+    public void restoreFileState() throws Exception {
+        //restoreTestDocument(docRestoreFile,USE_CASE_FILE_PATH);
+    }
+}
