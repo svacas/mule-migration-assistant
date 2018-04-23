97e482ea6f5c1db020476bc613767c76ee213751
Rodrigo Merino <elrodro83@users.noreply.github.com>
2018-04-23 10:49:34 -0300

    MMT-97: Create executable for the migrator (#66)

--- mule-migration-tool-tests/src/test/java/com/mulesoft/tools/migration/integration/EndToEndTestCase.java
+++ mule-migration-tool-tests/src/test/java/com/mulesoft/tools/migration/integration/EndToEndTestCase.java
@@ -15,12 +15,13 @@ import static org.mule.test.infrastructure.maven.MavenTestUtils.installMavenArti
 import org.mule.runtime.module.artifact.api.descriptor.BundleDescriptor;
 
 import com.mulesoft.mule.distributions.server.AbstractEeAppControl;
-import com.mulesoft.tools.migration.MigrationRunner;
 
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 
 /**
  * Tests the whole migration process, starting with a Mule 3 source config, migrating it to Mule 4, packaging and deploying it to
@@ -40,12 +41,19 @@ public abstract class EndToEndTestCase extends AbstractEeAppControl {
     File migrationResultFolder = migrationResult.newFolder(appName);
 
     // Run migration tool
-    // TODO Use the actual command line instead of running through java
-    MigrationRunner.main(new String[] {
-        "-projectBasePath", projectBasePath,
-        "-destinationProjectBasePath", migrationResultFolder.getAbsolutePath(),
-        "-migrationConfigurationPath", ""
-    });
+    ProcessBuilder pb = new ProcessBuilder("java", "-jar", getProperty("migrator.runner"),
+                                           "-projectBasePath", projectBasePath,
+                                           "-destinationProjectBasePath", migrationResultFolder.getAbsolutePath(),
+                                           "-migrationConfigurationPath", "");
+    pb.redirectErrorStream(true);
+    Process p = pb.start();
+
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println("Migrator: " + line);
+      }
+    }
 
     BundleDescriptor migratedAppDescriptor = new BundleDescriptor.Builder().setGroupId("org.mule.migrated")
         .setArtifactId(appName).setVersion("1.0.0-SNAPSHOT").setClassifier(MULE_APPLICATION_CLASSIFIER).build();
