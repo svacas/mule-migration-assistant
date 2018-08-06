6b301028c74ac23291281c44fe24c2472400480b
julianpascualMuleSoft <julian.pascual@mulesoft.com>
2018-08-06 12:11:27 -0300

    MMT-195 Ignore catalog folder and mule-project.xml

--- mule-migration-tool-engine/src/main/java/com/mulesoft/tools/migration/engine/project/structure/util/Exclusions.java
+++ mule-migration-tool-engine/src/main/java/com/mulesoft/tools/migration/engine/project/structure/util/Exclusions.java
@@ -19,12 +19,13 @@ import com.mulesoft.tools.migration.engine.project.structure.mule.three.MuleThre
  */
 public enum Exclusions {
 
-  CLASSPATH(".classpath"), GIT(".gitignore"), PROJECT(".project"), MULE(".mule"), SETTINGS(".settings"), TARGET(
-      "target"), MULE_THREE_APP(MuleThreeApplication.srcMainConfigurationPath), MULE_THREE_TEST(
-          MuleThreeApplication.srcTestsConfigurationPath), MULE_FOUR_APP(
-              MuleFourApplication.srcMainConfigurationPath), MULE_FOUR_TEST(
-                  MuleFourApplication.srcTestConfigurationPath), MULE_THREE_DOMAIN(
-                      MuleThreeDomain.srcMainConfigurationPath), MULE_FOUR_DOMAIN(MuleFourDomain.srcMainConfigurationPath);
+  CLASSPATH(".classpath"), GIT(".gitignore"), PROJECT(".project"), MULE(".mule"), SETTINGS(".settings"), CATALOG(
+      "catalog"), MULE_PROJECT("mule-project.xml"), TARGET("target"), MULE_THREE_APP(
+          MuleThreeApplication.srcMainConfigurationPath), MULE_THREE_TEST(
+              MuleThreeApplication.srcTestsConfigurationPath), MULE_FOUR_APP(
+                  MuleFourApplication.srcMainConfigurationPath), MULE_FOUR_TEST(
+                      MuleFourApplication.srcTestConfigurationPath), MULE_THREE_DOMAIN(
+                          MuleThreeDomain.srcMainConfigurationPath), MULE_FOUR_DOMAIN(MuleFourDomain.srcMainConfigurationPath);
 
   private String exclusion;
 
