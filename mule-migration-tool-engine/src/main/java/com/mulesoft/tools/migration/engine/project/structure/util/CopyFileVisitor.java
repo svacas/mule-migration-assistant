a0affcd962d070e1282ac407fb9b408a7d95131f
Julian Pascual <julian.pascual@mulesoft.com>
2018-08-06 11:02:26 -0300

    MMT-195 Ignore copying Eclipse folders during migration (#146)

--- mule-migration-tool-engine/src/main/java/com/mulesoft/tools/migration/engine/project/structure/util/CopyFileVisitor.java
+++ mule-migration-tool-engine/src/main/java/com/mulesoft/tools/migration/engine/project/structure/util/CopyFileVisitor.java
@@ -6,15 +6,15 @@
  */
 package com.mulesoft.tools.migration.engine.project.structure.util;
 
-import com.mulesoft.tools.migration.engine.project.structure.mule.four.MuleFourApplication;
-import com.mulesoft.tools.migration.engine.project.structure.mule.three.MuleThreeApplication;
-
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
  * A visitor of files
@@ -27,25 +27,15 @@ public class CopyFileVisitor implements FileVisitor<Path> {
   private final File fromFolder;
   private final File targetFolder;
 
-  private final List<Path> exclusions;
-
   public CopyFileVisitor(File fromFolder, File targetFolder) {
     this.fromFolder = fromFolder;
     this.targetFolder = targetFolder;
-    this.exclusions = new ArrayList<>();
-    this.exclusions.add(Paths.get(MuleThreeApplication.srcMainConfigurationPath));
-    this.exclusions.add(Paths.get(MuleThreeApplication.srcTestsConfigurationPath));
-    this.exclusions.add(Paths.get(MuleFourApplication.srcMainConfigurationPath));
-    this.exclusions.add(Paths.get(MuleFourApplication.srcTestConfigurationPath));
-
   }
 
   @Override
   public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-    for (Path excludePath : exclusions) {
-      if (dir.toString().contains(excludePath.toString())) {
-        return FileVisitResult.SKIP_SUBTREE;
-      }
+    if (doExclude(dir)) {
+      return FileVisitResult.SKIP_SUBTREE;
     }
     Path targetPath = targetFolder.toPath().resolve(fromFolder.toPath().relativize(dir));
     if (!Files.exists(targetPath)) {
@@ -56,8 +46,15 @@ public class CopyFileVisitor implements FileVisitor<Path> {
 
   @Override
   public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+    if (doExclude(file)) {
+      return FileVisitResult.SKIP_SUBTREE;
+    }
     Files.copy(file, targetFolder.toPath().resolve(fromFolder.toPath().relativize(file)), StandardCopyOption.REPLACE_EXISTING);
     return FileVisitResult.CONTINUE;
+  }
+
+  private Boolean doExclude(Path path) {
+    return Arrays.stream(Exclusions.values()).anyMatch(e -> path.toString().endsWith(e.exclusion()));
   }
 
   @Override
---
+++ mule-migration-tool-engine/src/main/java/com/mulesoft/tools/migration/engine/project/structure/util/Exclusions.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 MuleSoft, Inc. This software is protected under international
+ * copyright law. All use of this software is subject to MuleSoft's Master Subscription
+ * Agreement (or other master license agreement) separately entered into in writing between
+ * you and MuleSoft. If such an agreement is not in place, you may not use the software.
+ */
+package com.mulesoft.tools.migration.engine.project.structure.util;
+
+import com.mulesoft.tools.migration.engine.project.structure.mule.four.MuleFourApplication;
+import com.mulesoft.tools.migration.engine.project.structure.mule.four.MuleFourDomain;
+import com.mulesoft.tools.migration.engine.project.structure.mule.three.MuleThreeApplication;
+import com.mulesoft.tools.migration.engine.project.structure.mule.three.MuleThreeDomain;
+
+/**
+ * Files/Folders excluded from migration.
+ *
+ * @author Mulesoft Inc.
+ * @since 1.0.0
+ */
+public enum Exclusions {
+
+  CLASSPATH(".classpath"), GIT(".gitignore"), PROJECT(".project"), MULE(".mule"), SETTINGS(".settings"), TARGET(
+      "target"), MULE_THREE_APP(MuleThreeApplication.srcMainConfigurationPath), MULE_THREE_TEST(
+          MuleThreeApplication.srcTestsConfigurationPath), MULE_FOUR_APP(
+              MuleFourApplication.srcMainConfigurationPath), MULE_FOUR_TEST(
+                  MuleFourApplication.srcTestConfigurationPath), MULE_THREE_DOMAIN(
+                      MuleThreeDomain.srcMainConfigurationPath), MULE_FOUR_DOMAIN(MuleFourDomain.srcMainConfigurationPath);
+
+  private String exclusion;
+
+  Exclusions(String exclusion) {
+    this.exclusion = exclusion;
+  }
+
+  public String exclusion() {
+    return exclusion;
+  }
+
+}
