75c7a5dc60f9f24d3e9f1fe34b20433d69b45b07
Julian Pascual <julian.pascual@mulesoft.com>
2019-10-09 09:51:57 -0300

    MMT-363 Migrate processingStrategy defined on Configuration element (#315)

---
+++ mule-migration-tool-library/src/main/java/com/mulesoft/tools/migration/library/mule/steps/core/Configuration.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2017 MuleSoft, Inc. This software is protected under international
+ * copyright law. All use of this software is subject to MuleSoft's Master Subscription
+ * Agreement (or other master license agreement) separately entered into in writing between
+ * you and MuleSoft. If such an agreement is not in place, you may not use the software.
+ */
+package com.mulesoft.tools.migration.library.mule.steps.core;
+
+import com.mulesoft.tools.migration.step.AbstractApplicationModelMigrationStep;
+import com.mulesoft.tools.migration.step.category.MigrationReport;
+import org.jdom2.Element;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.mulesoft.tools.migration.step.util.TransportsUtils.COMPATIBILITY_NAMESPACE;
+import static com.mulesoft.tools.migration.step.util.XmlDslUtils.addCompatibilityNamespace;
+import static com.mulesoft.tools.migration.step.util.XmlDslUtils.getCoreXPathSelector;
+import static com.mulesoft.tools.migration.step.util.XmlDslUtils.getTopLevelCoreXPathSelector;
+
+/**
+ * Migrate Configuration element
+ *
+ * @author Mulesoft Inc.
+ * @since 1.0.0
+ */
+public class Configuration extends AbstractApplicationModelMigrationStep {
+
+  public static final String XPATH_SELECTOR = getTopLevelCoreXPathSelector("configuration");
+
+  @Override
+  public String getDescription() {
+    return "Update Configuration element.";
+  }
+
+  public Configuration() {
+    this.setAppliedTo(XPATH_SELECTOR);
+  }
+
+  @Override
+  public void execute(Element element, MigrationReport report) throws RuntimeException {
+    if (element.getAttribute("defaultProcessingStrategy") != null) {
+      element.removeAttribute("defaultProcessingStrategy");
+      report.report("configuration.processingStrategy", element, element);
+    }
+
+  }
+}
---
+++ mule-migration-tool-library/src/test/java/com/mulesoft/tools/migration/library/mule/steps/core/ConfigurationTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2017 MuleSoft, Inc. This software is protected under international
+ * copyright law. All use of this software is subject to MuleSoft's Master Subscription
+ * Agreement (or other master license agreement) separately entered into in writing between
+ * you and MuleSoft. If such an agreement is not in place, you may not use the software.
+ */
+package com.mulesoft.tools.migration.library.mule.steps.core;
+
+import com.mulesoft.tools.migration.project.model.ApplicationModel;
+import com.mulesoft.tools.migration.tck.ReportVerification;
+import org.apache.commons.io.IOUtils;
+import org.jdom2.Document;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.mulesoft.tools.migration.helper.DocumentHelper.getDocument;
+import static com.mulesoft.tools.migration.helper.DocumentHelper.getElementsFromDocument;
+import static com.mulesoft.tools.migration.tck.MockApplicationModelSupplier.mockApplicationModel;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.xmlunit.matchers.CompareMatcher.isSimilarTo;
+
+@RunWith(Parameterized.class)
+public class ConfigurationTest {
+
+  private static final Path CORE_CONFIG_EXAMPLES_PATH = Paths.get("mule/apps/core");
+
+  @Rule
+  public TemporaryFolder temp = new TemporaryFolder();
+
+  @Rule
+  public ReportVerification report = new ReportVerification();
+
+  @Parameterized.Parameters(name = "{0}")
+  public static Object[] params() {
+    return new Object[] {
+        "configuration-01"
+    };
+  }
+
+  private final Path configPath;
+  private final Path targetPath;
+
+  public ConfigurationTest(String filePrefix) {
+    configPath = CORE_CONFIG_EXAMPLES_PATH.resolve(filePrefix + "-original.xml");
+    targetPath = CORE_CONFIG_EXAMPLES_PATH.resolve(filePrefix + ".xml");
+  }
+
+  private Configuration configuration;
+
+  private Document doc;
+  private ApplicationModel appModel;
+
+  @Before
+  public void setUp() throws Exception {
+    doc = getDocument(this.getClass().getClassLoader().getResource(configPath.toString()).toURI().getPath());
+    appModel = mockApplicationModel(doc, temp);
+
+    configuration = new Configuration();
+    configuration.setApplicationModel(appModel);
+  }
+
+  @Test
+  public void execute() throws Exception {
+    getElementsFromDocument(doc, configuration.getAppliedTo().getExpression())
+        .forEach(node -> configuration.execute(node, report.getReport()));
+
+    XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
+    String xmlString = outputter.outputString(doc);
+
+    assertThat(xmlString,
+               isSimilarTo(IOUtils.toString(this.getClass().getClassLoader().getResource(targetPath.toString()).toURI(), UTF_8))
+                   .ignoreComments().normalizeWhitespace());
+  }
+}
