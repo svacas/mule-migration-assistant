b927d604c33c8793a74ef8d0cdd21e71912be467
svacas <svacas@gmail.com>
2022-05-24 15:10:27 -0300

    Support processor chains without name attribute (#641)

--- mule-migration-tool-library/src/main/java/com/mulesoft/tools/migration/library/mule/steps/core/ProcessorChain.java
+++ mule-migration-tool-library/src/main/java/com/mulesoft/tools/migration/library/mule/steps/core/ProcessorChain.java
@@ -9,6 +9,8 @@ import com.mulesoft.tools.migration.step.AbstractApplicationModelMigrationStep;
 import com.mulesoft.tools.migration.step.category.MigrationReport;
 import org.jdom2.Element;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static com.mulesoft.tools.migration.step.util.XmlDslUtils.CORE_NAMESPACE;
 import static com.mulesoft.tools.migration.step.util.XmlDslUtils.CORE_NS_URI;
 import static com.mulesoft.tools.migration.step.util.XmlDslUtils.addElementAfter;
@@ -25,6 +27,9 @@ public class ProcessorChain extends AbstractApplicationModelMigrationStep {
 
   public static final String XPATH_SELECTOR = "//*[namespace-uri()='" + CORE_NS_URI + "' and local-name()='processor-chain']";
 
+  private static final String UNNAMED_PROCESSOR_CHAIN_PREFIX = "MMA_processor_chain_";
+  private final AtomicInteger unnamedProcessorChainIndex = new AtomicInteger();
+
   @Override
   public String getDescription() {
     return "Update Processor Chain component.";
@@ -37,6 +42,9 @@ public class ProcessorChain extends AbstractApplicationModelMigrationStep {
   @Override
   public void execute(Element element, MigrationReport report) throws RuntimeException {
     String name = element.getAttributeValue("name");
+    if (name == null) {
+      name = UNNAMED_PROCESSOR_CHAIN_PREFIX + unnamedProcessorChainIndex.incrementAndGet();
+    }
     Element subFlow = new Element("sub-flow", CORE_NAMESPACE).setAttribute("name", name);
     subFlow.addContent(element.cloneContent());
 
